ea52b1f842c6df94e5c2e4118e4e0c35b50d4db6
be-garam <be.garam.dev@gmail.com>
2024-05-06 23:29:02 +0900

    ADD button in Main

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script>
-    import { Input, Helper} from 'flowbite-svelte';
+    import { Input, Helper, GradientButton} from 'flowbite-svelte';
 </script>
 
 <div class="flex items-center justify-center h-screen w-screen bg-neutral-100">
     <div class="flex flex-col space-y-10 items-center justify-center">
         <p class="text-6xl">Let's Fire School 🤜🔥🏫</p>
-        <div class="w-max max-w-4xl w-852 flex flex-col space-y-2 ">
-            <div class="w-full">
-                <Input type="url" id="website" placeholder="Enter School code to fire" required />
-            </div>
+        <div class=" flex flex-col space-y-2 w-100">
+            <div class="w-full flex flex-row space-x-2">
+                <div class="w-full">
+                    <Input type="url" id="website" placeholder="Enter School code to fire" required />
+                </div>
+                <GradientButton outline color="pinkToOrange" class="w-40">Fire🔥</GradientButton>
+            </div>  
             <Helper class="text-sm">
                 We’ll never share your details. Read our <a href="/" class="font-medium text-primary-600 hover:underline dark:text-primary-500"> Privacy Policy </a>
             </Helper>
