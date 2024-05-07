4d15082c3890b8dd610ee81b45c865c7706bc31c
be-garam <be.garam.dev@gmail.com>
2024-05-07 21:54:36 +0900

    ADD sidebar part

--- frontend/src/routes/result/+page.svelte
+++ frontend/src/routes/result/+page.svelte
@@ -1,14 +1,70 @@
 <script>
-    import {GradientButton} from 'flowbite-svelte';
-    import { goto } from '$app/navigation';
+    import {Card, Listgroup, GradientButton} from 'flowbite-svelte';
     
-    async function handleClick() {
-        goto('/result/+page.svelte'); // navigate to /loading/+page.svelte
-    }
+    let path = 'https://github.com/be-garam'
+    
+    let keyword_list = ['school', 'important', 'file'];
+
+    let url_list = [
+        {
+            name: 'url1',
+            path: path
+        },
+        {
+            name: 'url2',
+            path: path
+        },
+        {
+            name: 'url3',
+            path: path
+        }
+    ];
+
+    let file_list = [
+        {
+            name: 'file1',
+            path: path
+        },
+        {
+            name: 'file2',
+            path: path
+        },
+        {
+            name: 'file3',
+            path: path
+        }
+    ];
 </script>
 
-<div class="flex items-center justify-center h-screen w-screen bg-neutral-100">
-    <div class="flex flex-col space-y-10 items-center justify-center">
-        <p class="text-6xl">Result🔥</p>
+<div class="flex items-center justify-center h-screen w-screen bg-neutral-100 gap-x-24 p-24">
+    <div class="flex-none w-96 flex-col space-y-10">
+        <Card class="bg-white">
+            <h3 class="text-xl font-medium text-gray-900 dark:text-white">your school's keyword</h3>
+        </Card>
+        <Card class="bg-white">
+            <h3 class="text-xl font-medium text-gray-900 dark:text-white">Seems Important</h3>
+            <Listgroup items={url_list} let:item class="border-0 dark:!bg-transparent">
+                <div class="flex flex-row space-x-2 items-center">
+                    <p class="grow text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {item.name}
+                    </p>
+                    <GradientButton href={item.path} outline color="pinkToOrange" class="w-40">Fire🔥</GradientButton>
+                </div>
+            </Listgroup>
+        </Card>
+        <Card class="bg-white">
+            <h3 class="text-xl font-medium text-gray-900 dark:text-white">File we found</h3>
+            <Listgroup items={file_list} let:item class="border-0 dark:!bg-transparent">
+                <div class="flex flex-row space-x-2 items-center">
+                    <p class="grow text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {item.name}
+                    </p>
+                    <GradientButton href={item.path} outline color="pinkToOrange" class="w-40">Fire🔥</GradientButton>
+                </div>
+            </Listgroup>
+        </Card>
+    </div>
+    <div class="flex-1 bg-white">
+        03
     </div>
 </div>
