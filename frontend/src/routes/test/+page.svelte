69a88597ee2a6e2f53a8570e5bf624640ccc661b
be-garam <be.garam.dev@gmail.com>
2024-06-07 20:20:21 +0900

    CHECKED there is no issue with url

--- frontend/src/routes/test/+page.svelte
+++ frontend/src/routes/test/+page.svelte
@@ -1,27 +1,29 @@
 <script>
+    import {GradientButton} from 'flowbite-svelte';
+    let answer = "No answer yet";
     async function getAnswer() {
-        let url = "http://10.125.208.189:9241/v1/chat/completions"
+        let url = "http://10.125.208.189:9241/v1/models"
         const res = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                messages: [
-                {
-                    role: "school admissions specialist",
-                    content: chat
-                }
-                ],
-                model: "OpenBuddy/openbuddy-llama3-8b-v21.1-8k"
-            })
+            method: 'GET',
+            // body: JSON.stringify({
+            //     messages: [
+            //     {
+            //         role: "school admissions specialist",
+            //         content: "hi"
+            //     }
+            //     ],
+            //     model: "OpenBuddy/openbuddy-llama3-8b-v21.1-8k"
+            // })
         });
-        const data = await res.json();s
-        console.log(data);
+        console.log(res);
+        answer = await res.json();
+        console.log("Answer:");
+        console.log(answer);
     }
 </script>
 
+<GradientButton outline color="cyanToBlue" class="w-40" on:click={getAnswer}>Answer</GradientButton>
 <pre>
-    {data}
+    {answer}
 </pre>
 
