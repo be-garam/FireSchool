e7dd87d656c4105a74372d76a746f8356cc7acdb
be-garam <be.garam.dev@gmail.com>
2024-06-12 03:23:02 +0900

    ADD min-width

--- frontend/src/routes/result/+page.svelte
+++ frontend/src/routes/result/+page.svelte
@@ -4,6 +4,22 @@
     import { GradientButton, Badge, Input, ButtonGroup, P, Spinner } from 'flowbite-svelte';
     import { page } from '$app/stores';
 
+    // small screen
+    let isSmallScreen = false;
+
+    onMount(() => {
+        const updateScreenSize = () => {
+            isSmallScreen = window.innerWidth <= 1300;
+        };
+
+        window.addEventListener('resize', updateScreenSize);
+        updateScreenSize();
+
+        return () => {
+            window.removeEventListener('resize', updateScreenSize);
+        };
+    });
+
     // getting School name from URL
     let schoolData = null;
     let error = null;
@@ -119,83 +135,89 @@
             </div>
         </div>
     {:else}
-        <div class="flex items-center justify-center h-screen w-screen bg-neutral-100 divide-x-2">
-            <div class="flex-none w-80 h-full flex-col space-y-14 justify-start bg-white py-12 px-4">
-                <div class="space-y-6 flex-none">
-                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">🗝️ Your school's keyword</h3>
-                    <div class="flex flex-row space-x-2">
-                        {#each keyword_list as keyword (keyword)}
-                            <Badge large color="indigo">{keyword}</Badge>
-                        {/each}
-                    </div> 
-                </div>
-                <div class="space-y-6 flex-auto">
-                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">👀 Seems Important</h3>
-                    <div class="flex flex-col space-y-2">
-                        {#each url_list as item, index (index)}
-                            <div class="flex flex-row space-x-2 items-center rounded bg-slate-100 px-2 py-2">
-                                <p class="grow text-sm font-medium text-gray-900 truncate dark:text-white line-clamp-1">
-                                    {item}
-                                </p>
-                                <GradientButton href={item} color="cyanToBlue" size="xs">🌊 Surf</GradientButton>
-                            </div>
-                        {/each}
-                    </div>
-                </div>
-                <div class="space-y-6 flex-auto">
-                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">🗂️ Files we found</h3>
-                    <div class="flex flex-col space-y-2">
-                        {#each file_list as item, index (index)}
-                            <div class="flex flex-row space-x-2 items-center rounded bg-slate-100 px-2 py-2">
-                                <p class="grow text-sm font-medium text-gray-900 truncate dark:text-white line-clamp-1">
-                                    {item}
-                                </p>
-                                <GradientButton href={item} color="cyanToBlue" size="xs">🌊 Surf</GradientButton>
-                            </div>
-                        {/each}
-                    </div>
-                </div>
+        {#if isSmallScreen}
+            <div class="flex items-center justify-center h-screen w-screen bg-neutral-100">
+                <p class="text-6xl"> 🫷 Strech me!😖 🫸</p>
             </div>
-            <div class="flex-1 flex h-full flex-col space-y-4 py-12 px-64">
-                <div class="flex-auto flex-col space-y-4 overflow-y-auto">
-                    {#if Array.isArray(data.messages)}
-                        {#each data.messages as messages, index (index)}
-                            {#if messages.speaker == 'user'}
-                                <div class="w-full items-end flex">
-                                    <div class="w-2/5"></div>
-                                    <div class="w-3/5 p-4 bg-white rounded-lg border">
-                                        <P color="text-green-700 dark:text-green-500">{messages.speaker}</P>
-                                        {messages.message}
-                                    </div>
+        {:else}
+            <div class="flex items-center justify-center h-screen w-screen bg-neutral-100 divide-x-2">
+                <div class="flex-none w-80 h-full flex-col space-y-14 justify-start bg-white py-12 px-4">
+                    <div class="space-y-6 flex-none">
+                        <h3 class="text-xl font-medium text-gray-900 dark:text-white">🗝️ Your school's keyword</h3>
+                        <div class="flex flex-row space-x-2">
+                            {#each keyword_list as keyword (keyword)}
+                                <Badge large color="indigo">{keyword}</Badge>
+                            {/each}
+                        </div> 
+                    </div>
+                    <div class="space-y-6 flex-auto">
+                        <h3 class="text-xl font-medium text-gray-900 dark:text-white">👀 Seems Important</h3>
+                        <div class="flex flex-col space-y-2">
+                            {#each url_list as item, index (index)}
+                                <div class="flex flex-row space-x-2 items-center rounded bg-slate-100 px-2 py-2">
+                                    <p class="grow text-sm font-medium text-gray-900 truncate dark:text-white line-clamp-1">
+                                        {item}
+                                    </p>
+                                    <GradientButton href={item} color="cyanToBlue" size="xs">🌊 Surf</GradientButton>
                                 </div>
-                            {/if}
-                            {#if messages.speaker == 'bot'}
-                                <div class="w-full items-end flex">
-                                    <div class="w-3/5 p-4 bg-white rounded-lg border">
-                                        <P color="text-blue-700 dark:text-blue-500">{messages.speaker}</P>
-                                        {#if messages.message === '...'}
-                                            <Spinner color="blue" size="6"/>
-                                        {:else}
+                            {/each}
+                        </div>
+                    </div>
+                    <div class="space-y-6 flex-auto">
+                        <h3 class="text-xl font-medium text-gray-900 dark:text-white">🗂️ Files we found</h3>
+                        <div class="flex flex-col space-y-2">
+                            {#each file_list as item, index (index)}
+                                <div class="flex flex-row space-x-2 items-center rounded bg-slate-100 px-2 py-2">
+                                    <p class="grow text-sm font-medium text-gray-900 truncate dark:text-white line-clamp-1">
+                                        {item}
+                                    </p>
+                                    <GradientButton href={item} color="cyanToBlue" size="xs">🌊 Surf</GradientButton>
+                                </div>
+                            {/each}
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-1 flex h-full flex-col space-y-4 py-12 px-64">
+                    <div class="flex-auto flex-col space-y-4 overflow-y-auto">
+                        {#if Array.isArray(data.messages)}
+                            {#each data.messages as messages, index (index)}
+                                {#if messages.speaker == 'user'}
+                                    <div class="w-full items-end flex">
+                                        <div class="w-2/5"></div>
+                                        <div class="w-3/5 p-4 bg-white rounded-lg border">
+                                            <P color="text-green-700 dark:text-green-500">{messages.speaker}</P>
                                             {messages.message}
-                                        {/if}
+                                        </div>
                                     </div>
-                                    <div class="w-2/5"></div>
-                                </div>
-                            {/if}
-                        {/each}
-                    {:else}
-                        <p>No messages available</p>
-                    {/if}
-                </div>
-                <div class=flex-none>
-                    <form on:submit={handleSubmit}>
-                        <ButtonGroup class="w-full">
-                            <Input type="text" placeholder="💬 Chat here" size="lg" name="chat" bind:value={question} autocomplete="off"/>
-                            <GradientButton color="cyanToBlue" size="lg" type="submit">Send</GradientButton>
-                        </ButtonGroup>
-                    </form>
+                                {/if}
+                                {#if messages.speaker == 'bot'}
+                                    <div class="w-full items-end flex">
+                                        <div class="w-4/5 p-4 bg-white rounded-lg border">
+                                            <P color="text-blue-700 dark:text-blue-500">{messages.speaker}</P>
+                                            {#if messages.message === '...'}
+                                                <Spinner color="blue" size="6"/>
+                                            {:else}
+                                                {messages.message}
+                                            {/if}
+                                        </div>
+                                        <div class="w-1/5"></div>
+                                    </div>
+                                {/if}
+                            {/each}
+                        {:else}
+                            <p>No messages available</p>
+                        {/if}
+                    </div>
+                    <div class=flex-none>
+                        <form on:submit={handleSubmit}>
+                            <ButtonGroup class="w-full">
+                                <Input type="text" placeholder="💬 Chat here" size="lg" name="chat" bind:value={question} autocomplete="off"/>
+                                <GradientButton color="cyanToBlue" size="lg" type="submit">Send</GradientButton>
+                            </ButtonGroup>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        {/if}
     {/if}
 {/if}
